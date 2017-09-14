c911c19e766dbde56a1c1d7fd01b02c1d682c836
Mohammad Khan <sparrowangels@gmail.com>
2017-09-14 18:09:25 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+require_relative "./user"
+class Student < User
+  attr_accessor :knowledge
 
+  def initialize
+    @knowledge=[]
+  end
+
+  def learn(new_knowledge)
+    @knowledge << new_knowledge
+  end
 end
